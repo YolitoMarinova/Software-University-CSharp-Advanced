4f8368134438a861584c6e077718f4808048ab0d
Yoana <marinova.m.y@gmail.com>
2019-06-04 16:52:23 +0300

    exr.3 and exr.4

---
+++ Excercise/Streams, Files and Directories/03.WordCount/WordCount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class WordCount
+    {
+        public static void Main()
+        {
+            string text = File.ReadAllText("text.txt");
+            string[] words = File.ReadAllLines("words.txt");
+
+            Dictionary<string, int> results = new Dictionary<string, int>();
+
+            using (var writer = new StreamWriter("actualResult.txt"))
+            {
+                string[] lineContent = text.ToLower().Split(new char[] { ' ', ',', '.', '!', '?', '-', ':', ';', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    int count = lineContent.Count(x=>x==word);
+                    results.Add(word,count);
+                    writer.WriteLine($"{word} - {count}");
+                }
+            }
+
+            using (var writer = new StreamWriter("expectedResult.txt"))
+            {
+                foreach (var (key,value) in results.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{key} - {value}");
+                }
+            }
+        }
+    }
+}
---
+++ Excercise/Streams, Files and Directories/04.CopyBinaryFile/CopyBineryFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class CopyBineryFile
+    {
+        public static void Main()
+        {
+            string picPath = "copyMe.png";
+            string picCopyPath = "copyMe-copy.png";
+
+            using (FileStream reader=new FileStream(picPath,FileMode.Open))
+            {
+                using (FileStream writer=new FileStream(picCopyPath,FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] byteArray = new byte[5000];
+
+                        int size = reader.Read(byteArray, 0, byteArray.Length);
+
+                        if (size == 0) break;
+
+                        writer.Write(byteArray,0,size);
+                    }
+                }
+            }
+        }
+    }
+}
