31d6923ea9b4a3d1e248022ef6a05908e24e61a4
Yoana <marinova.m.y@gmail.com>
2019-06-04 14:38:55 +0300

    Streams, Files and Directories.sln

---
+++ Excercise/Streams, Files and Directories/01.EvenLines/EvenLines.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01.EvenLines
+{
+    class EvenLines
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader(@"text.txt"))
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line == null) break;
+
+                    line = ReplaceSymbols(line);
+                    string[] reverseWords = line.Split().Reverse().ToArray();
+                    Console.Write(String.Join(" ", reverseWords));
+                    Console.WriteLine();
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            return line.Replace("-", "@")
+                .Replace(",", "@")
+                .Replace(".", "@")
+                .Replace("!", "@")
+                .Replace("?", "@");
+        }
+    }
+}
---
+++ Excercise/Streams, Files and Directories/02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class LineNumbers
+    {
+        public static void Main()
+        {
+            string[] textLines=File.ReadAllLines("text.txt");
+
+            int counter = 1;
+
+            foreach (var currentLine in textLines)
+            {
+                int lettersCount = currentLine.Count(char.IsLetter);
+                int punctuationsCount = currentLine.Count(char.IsPunctuation);
+
+                File.AppendAllText("output.txt",$"Line {counter}: {currentLine} ({lettersCount})({punctuationsCount})" +
+                    $"{Environment.NewLine}");
+
+                counter++;
+            }
+        }
+    }
+}
