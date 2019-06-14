d7637900fe20c40e2aa29f7498835024a882124a
Yoana <marinova.m.y@gmail.com>
2019-06-15 00:43:52 +0300

    Ex.10

---
+++ Excercise/Defining Classes/10.SoftUniParking/Parking.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars ;
+        private int capacity;
+        public int Count { get { return cars.Count; } }
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+
+            cars = new Dictionary<string, Car>();
+        }
+
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (cars.Count == capacity)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(car.RegistrationNumber, car);
+
+            return $"Successfully added new car {car.Make} { car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            cars.Remove(registrationNumber);
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            Car car = null;
+
+            if (cars.ContainsKey(registrationNumber))
+            {
+                car = cars[registrationNumber];
+            }
+
+            return car;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                RemoveCar(registrationNumber);
+            }
+        }
+
+    }
+}
---
+++ Excercise/Defining Classes/10.SoftUniParking/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Parking parking = new Parking(2);
+
+            Car car = new Car("Skoda","Fabia",65,"CC1856BG");
+
+            Console.WriteLine(parking.AddCar(car));
+
+            Console.WriteLine(parking.GetCar(car.RegistrationNumber));
+
+
+        }
+    }
+}
