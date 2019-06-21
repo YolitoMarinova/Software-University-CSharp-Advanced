eaa7ff28c05ff04560ef356b38bca96310fe4fca
Yoana <marinova.m.y@gmail.com>
2019-06-21 12:05:26 +0300

    Iterators and Comparators.sln

---
+++ Lab/Iterators and Comparators/01.Library/StartUp.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+
+namespace IteratorsAndComparators
+{
+   public class StartUp
+    {
+       public static void Main()
+        {
+            Book firstBook = new Book("Proizhod",2018,"Daun Brown");
+            Book secondBook = new Book("To", 2018, "Steaven King");
+            Book thirdBook = new Book("The Secret", 2018, "Random");
+
+            Library firstLibrary = new Library();
+            Library secondLibrary = new Library(firstBook,secondBook,thirdBook);
+
+            foreach (var book in secondLibrary)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine(secondBook.CompareTo(secondBook));
+        }
+    }
+}
